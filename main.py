2d0b3e5c05518641273c7c90fcadeb4b75de55e4
veds <vishalratna7600@gmail.com>
2023-01-01 00:12:02 +0530

    update 2.0

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
-from values import cards
+from values import cards, points
 import random
 from logo import logo
 
-def display(st):
-    
-    print("\n \n")
-    if st == "lose1":
-        print("YOU LOST!!!!! ")
-        print("you lost because you had too many numbers")
-        print(f"computer has {computer}\t total = {sum(computer)}")
-        print(f"you have {user} \t total = {sum(user)}")
-    elif st == "lose2":
-        print("YOU LOST!!!!! ")
-        print("computer had more numbers than you")
-        print(f"computer has {computer}\t total = {sum(computer)}")
-        print(f"you have {user} \t total = {sum(user)}")
-    elif st == "win":
-        print("YOU WIN CONGRATS!!!!! ")
-
-        print("congrats on your win")
-        print(f"computer has {computer}\t total = {sum(computer)}")
-        print(f"you have {user} \t total = {sum(user)}")
-    
-        
-        
-
 def get_input():
+    print(f"computer has: {computer[0]}")
+    print(f"you have: {user} \t total = {sum(user)}")        
+    print("\n \n")
+
+    if sum(user) == 21:
+        display("win")
     chooice = input("hit or stand: ")
     deal(chooice)
     
+def add_computer():
+    computer.pop()
+    computer.append(random.choice(cards))
+    if sum(computer) > 21:
+      add_computer()
+    else:
+        return
     
 def deal(choice):
     #hit condition
@@ -39,22 +29,23 @@ def deal(choice):
         else:
             user.append(a)
         print("\n")
-        print(f"computer has {computer[0]}")
-        print(f"you have {user} \t total = {sum(user)}")
         if sum(user) > 21: 
             display("lose1")
+        
         else:
             get_input()
     
     #stand condition
     elif choice == "stand":
-        print(f"computer has {computer}\t total = {sum(computer)}")
+        print("\n ")
         if sum(computer) < 17:
+            print(f"computer has {computer}\t total = {sum(computer)}")
             computer.append(random.choice(cards))
+            if sum(computer) > 21:
+                add_computer()
             print(f"after adding more,computer has {computer}\t total = {sum(computer)}")
-        
-        print(f"you have {user} \t total = {sum(user)}")        
-        print("\n \n")
+           
+        print("\n")
 
         
         if sum(user) > sum(computer):
@@ -64,6 +55,31 @@ def deal(choice):
         else:
             print("draw")
         
+def display(st):
+    global wager
+    global points
+    print(f"computer has {computer}\t total = {sum(computer)}")
+    print(f"you have {user} \t total = {sum(user)}")
+    print("\n \n")
+    if st == "lose1":
+        print("YOU LOST!!!!! ")
+        points -= wager
+        print("you lost because you had too many numbers")
+        
+    elif st == "lose2":
+        print("YOU LOST!!!!! ")
+        points -= wager
+        print("computer had more numbers than you")
+    elif st == "win":
+        print("YOU WIN CONGRATS!!!!! ")
+        points += wager
+        print("congrats on your win")
+            
+    inp = input("play again? ")
+    if inp == "yes":
+        print("you have: ", points)
+        wager = int(input("how much do you wager? "))
+        get_input()
         
 
 computer = [random.choice(cards), random.choice(cards)]
@@ -71,13 +87,12 @@ user = [random.choice(cards), random.choice(cards)]
 
 print(logo)
 
-#printing user's cards and one of computer's cards
-print(f"computer has: {computer[0]}")
-print(f"you have: {user} \t total = {sum(user)}")        
-print("\n \n")
+print("you have: ", points)
 
 
 #ask the user to hit or stand
+wager = int(input("how much do you wager? "))
+
 get_input()
 
 
